8c30939c335952a5e1a8b01276abfc400bf5066d
jose <joanluk@gmail.com>
2018-12-31 10:18:12 +0100

    Initial commit

---
+++ src/test/java/org/emaginalabs/samples/demopreauth/DemoPreauthApplicationTests.java
@@ -0,0 +1,17 @@
+package org.emaginalabs.samples.demopreauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoPreauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
